b2da6b3c697b8852214639b76a061bf8133484ba
Wen-tzu LO <wlo@e1r11p7.42.fr>
2022-07-08 17:06:06 +0200

    if_int

--- utils.hpp
+++ utils.hpp
@@ -1,6 +1,15 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
+namespace ft {
+    template <class T> 
+    struct is_integral : public integral_constant<bool, is_integral(T)>{
+        typedef bool value_type;
+        typedef 
 
+    }
+
+
+}
 
 
 #endif
