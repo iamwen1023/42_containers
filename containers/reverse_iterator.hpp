c4ac8e0aaebef8d1304db815c1962e0602a5d80f
Wen-tzu LO <wlo@e1r9p6.clusters.42paris.fr>
2023-01-17 12:03:22 +0100

    iterator

--- containers/reverse_iterator.hpp
+++ containers/reverse_iterator.hpp
@@ -1,5 +1,20 @@
 #ifndef REVERSE_ITERATOR_HPP
 #define REVERSE_ITERATOR_HPP
+namespace ft {
+    template< class Iter >
+    class reverse_iterator{
+        public:
+            typedef Iter iterator_type	;
+            typedef ft::iterator_traits<Iter>::iterator_category iterator_category;
+            typedef ft::iterator_traits<Iter>::value_type value_type;
+            typedef ft::iterator_traits<Iter>::difference_type difference_type;
+            typedef ft::iterator_traits<Iter>::pointer pointer;
+            typedef ft::iterator_traits<Iter>::reference reference;
+            reverse_iterator():current(){}
+            explicit reverse_iterator(iterator_type x ):current(x){}
+            template< class U >
+            reverse_iterator( const reverse_iterator<U>& other );
 
-
+    };
+}
 #endif
