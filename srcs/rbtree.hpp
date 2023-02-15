799b0dad934e5b29e9878fbbc051ec2d159308da
Wen-tzu LO <wlo@e1r4p7.clusters.42paris.fr>
2023-02-15 16:01:23 +0100

    delete rbtree

--- srcs/rbtree.hpp
+++ srcs/rbtree.hpp
@@ -44,13 +44,14 @@ class RBTree {
             }
         }
         //postorder
-         void inorderHelper(NodePtr node){
+        void inorderHelper(NodePtr node){
             if (node != TNULL){
                 preorderHelper(node->left);
                 preorderHelper(node->right);
                 std::cout << node->data << " ";
             }
         }
+
         NodePtr searchTreeHelp(NodePtr node, int value){
             if (node== TNULL || value =  node->data)
                 return node;
@@ -89,6 +90,7 @@ class RBTree {
             y->right = x;
             x->parent = y;
         }
+
         void insert(int key){
             NodePtr node =  new node;
             node->data = key;
@@ -103,7 +105,7 @@ class RBTree {
                 if (node->data < x->data)
                     x = x->left;
                 else
-                    x = x->left;
+                    x = x->right;
             }
             node->parent = y;
             if(y == nullptr)
@@ -118,7 +120,97 @@ class RBTree {
                 return ;
             insertFIX(node);
         }
-        
+        void insertFIX(NodePtr node){
+            NodePtr u;
+            while(node->parent->color == 'R'){
+                if(node->parent == node->parent->parent->right){
+                    u = node->parent->left;
+                    if(u->color == 'R'){
+                        node->parent->color = 'B';
+                        u->color = 'B';
+                        node->parent->parent = 'R';
+                        node = node->parent->parent;
+                    }else {
+                        if (node == node->parent->right){
+                            node = node->parent;
+                            leftRotate(node);
+                        }
+                        node->parent->color = 'B';
+                        node->parent->parent->color = 'R';
+                        rightRotate(node->parent->parent);
+                    }
+                } else{
+                    u = node->parent->right;
+                    if(u->color == 'R'){
+                        node->parent->color = 'B';
+                        u->color = 'B';
+                        node->parent->parent = 'R';
+                        node = node->parent->parent;
+                    }else {
+                        if (node == node->parent->right){
+                            node = node->parent;
+                            leftRotate(node);
+                        }
+                        node->parent->color = 'B';
+                        node->parent->parent->color = 'R';
+                        rightRotate(node->parent->parent);
+                    }
+                }
+                if(node == root)
+                    break ;
+            }
+            root->color = 'B';
+        }
+        NodePtr minimum(NodePtr node){
+            while(node->left != TNULL){
+                node = node->left;
+            }
+            return node;
+        }
+        NodePtr maximum(NodePtr node){
+            while(node->right != TNULL){
+                node = node->right;
+            }
+            return node;
+        }
+        void transplant(NodePtr u, NodePtr v){
+            if(u->parent == TNULL)
+                this->root = v;
+            else if(u == u->parent->left)
+                u->parent->left = v;
+            else
+                u->parent->right = v;
+            v->parent = u->parent;
+        }
+        void RB_delete(NodePtr node){
+            NodePtr y = node;
+            NodePtr x;
+            char color_origin = y->color;
+            if (node->left == TNULL){
+                x = node->right;
+                transplant(node, node->right); 
+            } else if (node->right == TNULL){
+                x = node->left
+                transplant(node, node->left);
+            } else {
+                y = minimum(node->right);
+                color_origin = y->color;
+                x = y->right;
+                if(y->parent == node)
+                    x->parent = y;
+                else {
+                    transplant(y, y->right);
+                    y->right = node->right;
+                    y->left->parent = y;
+                    transplant(node, y);
+                    y->left = node->left;
+                    y->left->parent = y;
+                    y->color = node->color;
+                }
+            }
+            if (color_origin = 'B');
+                deleteFix(x);
+        }
 
 }
 
