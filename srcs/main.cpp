dffa7b993eeac31efa54202f4f3dcdf694fdb105
Wen-tzu LO <wlo@e1r5p7.clusters.42paris.fr>
2023-02-27 16:01:40 +0100

    [200~zsh: segmentation fault (core dumped)  valgrind ./a.out~

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,9 +1,43 @@
 #include "./map.hpp"
 #include <iostream>
 
+template <typename Pair>
+struct SelectFirst {
+    typedef typename Pair::first_type result_type;
+    const result_type& operator()(const Pair& p) const {
+        return p.first;
+    }
+    result_type& operator()(Pair& p) const {
+        return p.first;
+    }
+};
+
+template <class Key, class T, class Compare = std::less<Key>>
+class pair_compare : std::less<Key>{
+    protected:
+    Compare _comp;
+    pair_compare(Compare c) : _comp(c) {}
+
+        public:
+                    // This so the RBTree can construct the class
+        pair_compare(void) : _comp(Compare()) {}
+
+        typedef Key                                         key_type;
+        typedef T                                           mapped_type;
+        typedef std::pair<const key_type, mapped_type>           value_type;
+
+        typedef value_type  first_argument_type;
+        typedef value_type  second_argument_type;
+                    
+        bool operator()(const value_type &x, const value_type &y)const
+        { return _comp(x.first, y.first); }
+};
+
 int main(){
-    RedBlackTree bst;
-    bst.insert(55);
+    rb_tree<std::pair<int,char>, pair_compare<int, char> >  bst;
+    bst.insert(std::make_pair(1,'a'));
+    bst.insert(std::make_pair(2,'a'));
+    bst.insert(std::make_pair(3,'a'));
     bst.printTree();
 
 }
--- srcs/map.hpp
+++ srcs/map.hpp
@@ -2,16 +2,24 @@
 #define MAP_HPP
 #include <functional>
 #include <memory>
+#include <iostream>
+#include <map>
+enum color_type {RED, BLACK};
 
 template <class Value>
 struct rb_tree_node{
     typedef rb_tree_node* base_ptr;
-    enum color_type {RED, BLACK};
     color_type color;
     base_ptr parent;
     base_ptr left;
     base_ptr right;
     Value value_field;
+
+    rb_tree_node(): parent(NULL), left(NULL), right(NULL),color(BLACK), value_field(NULL){
+    }
+    rb_tree_node(Value value): parent(NULL), left(NULL), right(NULL),color(RED), value_field(value){
+
+    }
 
     static base_ptr minimum(base_ptr x){
         while(x->left != 0)
@@ -25,71 +33,199 @@ struct rb_tree_node{
     } 
 };
 
-template <class Key, class Value, class KeyOfValue, class Compare = std::less<Value>>
+template <class Value, class Compare>
 class rb_tree{
-    protected:
-
-        typedef rb_tree_node<Value> rb_tree_node;
-        typedef typename std::allocator::template rebind<rb_tree_node<Value>>::other node_allocator;
+    //protected:
     public:
+
+        typedef typename std::allocator<Value>::template rebind<rb_tree_node<Value> >::other node_allocator;
+    
         typedef Value value_type;
-        typedef Key key_type;
-        typedef value_type* pointer;
-        typedef const value_type const_pointer;
+        typedef Compare compare_type;
         typedef value_type& reference;
-        typedef const value_type& const_reference;
-        typedef rb_tree_node* link_type;
+        // typedef const value_type& const_reference;
+        typedef rb_tree_node<Value> node_type;
+        typedef node_type           *node_ptr;
         typedef size_t size_type;
-        typedef ptrdiff_t difference_type;
-    protected:
-        link_type       root;
-        link_type       tnull;
+        // typedef ptrdiff_t difference_type;
+    //protected:
+        node_ptr       root;
+        node_ptr       tnull;
         size_type       node_count;
-        Compare         comp;
+        compare_type    comp;
         node_allocator  node_alloc;
 
-        void init(){
-
-        }
+        
 
         //construct
-        rb_tree(const Compare& comp= Compare): root(NULL), node_count(0),comp(comp), node_alloc(node_allocator()){}
-        link_type creat_node(const value_type& x){
-            link_type new = node_alloc.allocator(1);
-            node_alloc.construct(&new->value_field, x)
-            return new;
+        rb_tree(const compare_type& comp= Compare()):node_count(0),comp(comp), node_alloc(node_allocator()){
+            tnull =  node_ptr();
+            root = tnull;
+            std::cout << "default" << std::endl;
         }
-        void insert(const value_type& x){
-            link_type y = 0;
-            link_type x = root;
-            if(root != NULL){
-                y =x;
-                if()
-            }
+        ~rb_tree(){
+            clear_tree(root);
+        }
+        node_type *creat_node(const value_type& x){
+            node_type *new_node = node_alloc.allocate(1);
+            node_alloc.construct(new_node, x);
+            new_node->left = tnull;
+            new_node->right = tnull;
+            return new_node;
+        }
+        void put_node(node_ptr node){
+            node_alloc.destroy(node);
+            node_alloc.deallocate(node, 1);
         }
 
-        void printTree() {
+        void leftRotate(node_ptr x){
+            node_ptr y = x->right;
+            x->right = y->left;
+            if( y->left != tnull)
+                y->left->parent = x;
+            y->parent = x->parent;
+            if(x->parent == NULL)
+                this->root = y;
+            else if(x == x->parent->left)
+                x->parent->left = y;
+            else
+                x->parent->right = y;
+            y->left = x;
+            x->parent = y;
+        }
+        void rightRotate(node_ptr x){
+            node_ptr y = x->left;
+            x->left = y->right;
+            if(y->right != tnull)
+                y->right->parent = x;
+            y->parent = x->parent;
+            if(x->parent == NULL)
+                this->root = x;
+            else if (x == x->parent->left)
+                x->parent->left = y;
+            else
+                x->parent->right = y;
+            y->right = x;
+            x->parent = y;
+        }
+        void insert(const value_type& new_value){
+            node_type *new_node = creat_node(new_value);
+            node_type *y = NULL;
+            node_type *x = root;
+            while (x != tnull){
+                y = x;
+                if(comp(new_node->value_field, x->value_field) == true){
+                    x = x->left;
+                } else if (comp(new_node->value_field, x->value_field) == false && (comp(x->value_field ,new_node->value_field) == false)){
+                    std::cout << "inside value is the same" << std::endl;
+                    put_node(new_node);
+                    return ;
+                } else {
+                    x = x->right;
+                }
+            }
+            new_node->parent = y;
+            if ( y == NULL){
+                root = new_node;
+            } else if (comp(new_node->value_field, y->value_field)){
+                y->left = new_node;
+            } else {
+                y->right = new_node;
+            }
+            if (new_node->parent == NULL){
+                new_node->color = BLACK;
+                return ;
+            }
+            if (new_node->parent->parent == NULL)
+                return;
+            ++node_count;
+            insertFIX(new_node);
+        }
+
+        void    insertFIX(node_ptr node){
+            node_ptr u;
+
+            while(node->parent->color == RED){
+                if (node->parent == node->parent->parent->right){
+                    std::cout << "hello1\n";
+                    u = node->parent->parent->left;
+                    u->color == RED;
+                     std::cout << "hello1\n";
+                    if(u->color == RED){
+                         std::cout << "hello0\n";
+                        u->color = BLACK;
+                        node->parent->color = RED;
+                        node->parent->parent->color = BLACK;
+                        node =  node->parent->parent;
+                    } else {
+                        std::cout << "hello1\n";
+                        if (node == node->parent->left){
+                            std::cout << "hello1\n";
+                            node = node->parent;
+                            rightRotate(node);
+                        }
+                        node->parent->color = BLACK;
+                        node->parent->parent->color = RED;
+                        leftRotate(node->parent->parent);
+                    }
+                } else {
+                    u = node->parent->parent->right;
+                    if(u->color == RED){
+                        u->color = BLACK;
+                        node->parent->color = RED;
+                        node->parent->parent->color = BLACK;
+                        node =  node->parent->parent;
+                    } else {
+                        if (node == node->parent->right){
+                            node = node->parent;
+                            leftRotate(node);
+                        }
+                        node->parent->color = BLACK;
+                        node->parent->parent->color = RED;
+                        rightRotate(node->parent->parent);
+                    }
+                }
+                if (node == root)
+                    break ;
+            }
+            root->color = BLACK;
+        }
+
+        void    printTree() {
             if (root) 
                 printHelper(this->root, "", true);
+            else 
+                std::cout << "not root!\n";
         }
-        void printHelper(NodePtr root, string indent, bool last) {
+        void printHelper(node_ptr root, std::string indent, bool last) {
             if (root != 0) {
-                    cout << indent;
+                    std::cout << indent;
                 if (last) {
-                    cout << "R----";
+                    std::cout << "R----";
                     indent += "   ";
                 } else {
-                    cout << "L----";
+                    std::cout << "L----";
                     indent += "|  ";
                 }
 
-            string sColor = root->color ? "RED" : "BLACK";
-            cout << root->value_field << "(" << sColor << ")" << endl;
+            std::string sColor = root->color ? "RED" : "BLACK";
+            std::cout << root->value_field.first << "|"  << root->value_field.second <<  "(" << sColor << ")" << std::endl;
             printHelper(root->left, indent, false);
             printHelper(root->right, indent, true);
             }
         }
-}
+        void clear_tree(node_ptr subtree){
+            if (subtree){
+                node_ptr next_left = subtree->left;
+                node_ptr next_right = subtree->right;
+                node_alloc.destroy(subtree);
+                node_alloc.deallocate(subtree, 1);
+                node_count--;
+                clear_tree(next_left);
+                clear_tree(next_right);
+            }
+        }
+};
 
 // namespace ft {
 
